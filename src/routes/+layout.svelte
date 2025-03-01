f45ec230044250677d6bc4df2ed734951d944fba
Salty876 <100157151+Salty876@users.noreply.github.com>
2025-03-01 13:15:41 -0700

    Added the log in

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+
+    <div class="header">
+
+        <h3>Generic Bus time App</h3>
+    </div>
+
+
+<slot></slot>
+
+
+<style>
+
+
+    
+    :root{
+        padding: 0;
+        border: 0;
+        --main-color: #3d8c40;
+
+        background-color: #1f2937;
+        width: 100%;
+
+    }
+
+    h3{
+        color: white;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .header{
+        width: 100%;
+        height: 5vh;
+        background-color: var(--main-color);
+        padding: 20px 10px;
+        margin-top: -7px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .header h3{
+        padding-left: 20px;
+    }
+
+    @media only screen and (max-width: 600px) {
+        :root{
+            margin: 0;
+
+        }
+}       .header{
+        padding: -30px 0;
+}
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,6 +5,13 @@
         let password
         let message
 
+        
+
+        if (currentUser == true){
+            window.location.href = "/"
+
+        }
+
         async function login(){
         try{
             const logIn = await pb.collection('users').authWithPassword(email,password)
@@ -12,9 +19,8 @@
             window.location.href = "/"
         }
         catch {
-            message = 'Username or Password is incorrect'
-            console.log(message)
-      
+            message = "Email or password are invalid."
+            
             
         }
     }
@@ -32,8 +38,18 @@
 
        }
 
-       const createdUser = await pb.collection('users').create(data)
+       try{
+        const createdUser = await pb.collection('users').create(data)
        await login()
+
+       }  
+       catch {
+            message = "Check your email or password. Passwords must be larger than 8 characters."
+            
+            
+        }
+
+       
 
      
    }  
@@ -44,28 +60,106 @@
 
 </script>
 
-{#if $currentUser}
-    <h1>Were so back</h1>
-{/if}
 
-<form>
-    <input
-    placeholder="email"
-    type="text"
-    bind:value={email}
-    >
-    <input
-    placeholder="password"
-    type="text"
-    bind:value={password}
-    >
 
+<div class="main-site">
+
+    {#if $currentUser}
+    {/if}
+
+    <h1>Log in or register</h1>
+    <form>
+        <input
+        placeholder="email"
+        type="text"
+        class="text-input"
+        bind:value={email}
+        >
+        <input
+        placeholder="password"
+        type="password"
+        class="text-input"
+
+        bind:value={password}
+        >
     
-    <button on:click={signup}>
-        sign up
-    </button>
+        <div class="buttons">
+            <button on:click={signup}>
+                sign up
+            </button>
+        
+            <button on:click={login}>
+                log in
+            </button>
 
-    <button on:click={login}>
-        log in
-    </button>
-</form>
+        </div>
+
+    </form>
+
+    <h4>{message}</h4>
+
+</div>
+<style>
+
+    h1{
+        color: white;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .main-site {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        
+        height: 70vh;
+        width: 100%;
+        }
+
+    form{
+        width: 50%;
+        gap: 10px;
+        height: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 10px;
+    }
+    .buttons{
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        height: 15%;
+    }
+
+    button{
+        width: 49%;
+        background-color: var(--main-color);
+        border: none;
+        border-radius: 7px;
+
+        color: white;
+        font-size: larger;
+
+    }
+
+    input{
+        height: 10%;
+    }
+    input:focus{
+        outline: none;
+    }
+    .text-input{
+        height: 10%;
+    }
+     h4{
+        color: red;
+        font-family: Arial, Helvetica, sans-serif;
+     }
+    @media only screen and (max-width: 600px) {
+    form{
+        width: 80%;
+        justify-content:right;
+    }
+}
+</style>
